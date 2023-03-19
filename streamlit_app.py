f369a25852294856e584e18d763a446fe8c70df7
Igor Bakhtin <ibakhtin@nobilis.team>
2023-03-19 15:04:53 +0300

    added upload input

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from PIL import Image
+import torch
+import numpy as np
+
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+import av
 
 st.title("""Детектор мусора по фотографиям пользователей""")
 st.text("""Выполнили студенты Бахтин""")
+
+uploaded_file = st.file_uploader("Choose a file")
